9a91c8014e1d27c456ac481b18bf1692da91f18e
xzhang49 <xzhanguh@gmail.com>
2023-04-13 09:52:44 -0500

    Netflix Clone

---
+++ netflix-clone/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
